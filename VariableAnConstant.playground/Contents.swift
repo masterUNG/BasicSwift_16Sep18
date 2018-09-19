1d719ac707f7d3effa1cbe2592c420395a49d0e9
masterUNG <phrombutr@gmail.com>
2018-09-19 10:29:18 +0700

    Variable ans Constant

---
+++ VariableAnConstant.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+//Variable and Constant
+
+//String
+var nameString = "Master UNG"   //นี่คือการประกาศค่าคงที่
+let surnameString: String = "Phorombutr"   //Constant Explicit by Type
+
+nameString = "Doramon"
+
+//Integer
+let number1Int = 100        //Implicit Integer Type
+let number2Int: Int = 500   //Explicit Config Data Type
+
+var answer1Int: Int = number1Int + number2Int
+
+//Double
+let number3Dou = 12.23  // Implicit Double Type
+let number4Dou: Double = 100    //Explicit Double Typo
+
+let answer2Dou: Double = number3Dou - number4Dou
+
+//Boolean
+let statusABool = true
+let statusBBool: Bool = false
+
+let answer3Bool = !statusBBool
+
+
+
+
+
+
+
+
+
+
+
+
